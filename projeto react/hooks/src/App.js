9633e71fccf21bd71df5f7c4c781f93e6f21d9da
GabeDuarte <83724430+GabeDuarte@users.noreply.github.com>
2021-08-03 16:52:44 -0300

    first commit

---
+++ projeto react/hooks/src/App.js
@@ -0,0 +1,43 @@
+import React, {useState, useEffect} from 'react';
+
+function App(){
+  // tarefas  -> nome do state
+  // setTarefas -> função para atualizar o valor do state
+  const [tarefas, setTarefas] = useState([
+    'Estudar',
+    'Trabalhar',
+    'Cozinhar',
+    'Dar banho no cachorro',
+    'Assistir bob esponja',
+    'Plantar tomates'
+  ]);
+
+  const [nome, setNome] = useState('To Do List - 03/08/2021');
+
+  const [input, setInput] = useState('');
+
+  useEffect(()=> {localStorage.setItem('tarefas', JSON.stringify(tarefas))}, [tarefas]);
+  useEffect(()=> {const tarefasStorage })
+
+  function handleAdd(){
+    setTarefas([...tarefas, input]);
+    setInput('')
+  }
+
+  return (
+    <>
+    <ul>
+    <h2>{nome}</h2>
+      {tarefas.map((tarefa)=>(
+        <li key= {tarefa}>{tarefa}</li>
+      ))}
+    </ul>
+    <input placeholder='Insira novas tarefas' type='text' value={input} onChange={(e)=> setInput(e.target.value)}/>
+    <button type='button' onClick={handleAdd}>Adicionar</button>
+    </>
+  )
+}
+
+
+
+export default App;
